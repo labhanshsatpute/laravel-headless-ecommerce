0208f0c316e1fe62a1b70b2077dab8f28f9b93b8
Labhansh Satpute <73733885+labhanshsatpute@users.noreply.github.com>
2023-12-17 13:04:00 +0530

    changes

--- app/Http/Resources/API/ProductMediaResource.php
+++ app/Http/Resources/API/ProductMediaResource.php
@@ -15,6 +15,7 @@ class ProductMediaResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id" => $this->id,
             "priority" => $this->size,
             "type" => $this->type,
             "path" => asset('storage/'. $this->path)
--- app/Http/Resources/API/ProductSizeResource.php
+++ app/Http/Resources/API/ProductSizeResource.php
@@ -15,6 +15,7 @@ class ProductSizeResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id" => $this->id,
             "size" => $this->size,
             "price_original" => $this->price_original,
             "price_discounted" => $this->price_discounted
